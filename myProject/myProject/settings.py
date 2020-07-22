5a33f17eab852898be2c3d12b6a0de1baaae128c
zahrakoohestani <zahrakoohestani11@gmail.com>
2020-07-22 20:42:01 +0430

    edition of template

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -121,3 +120,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
